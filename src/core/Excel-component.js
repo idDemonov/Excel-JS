8024253f2e92cd63c5e23655a7e95f3bba29a064
idDemonov <rustamdemonov@gmail.com>
2020-05-17 02:59:21 +0300

    Настройка базовых методов

--- src/core/Excel-component.js
+++ src/core/Excel-component.js
@@ -1,7 +1,20 @@
 import { DomListener } from '@core/Dom-listener';
 
 export class ExcelComponent extends DomListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners);
+    this.name = options.name || '';
+  }
+
   toHTML() {
-    return '<HTNL>';
+    return '';
+  }
+
+  init() {
+    this.initDOMListeners();
+  }
+
+  destroy() {
+    this.removeDOMListeners();
   }
 }
