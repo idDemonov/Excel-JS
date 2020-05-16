7f0fb1ff2ec3b0dc083cdd9a36d8d2d13d6a6b5c
idDemonov <rustamdemonov@gmail.com>
2020-05-16 16:26:41 +0300

    функционал: создание, очистка и вставка элементов

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,9 +1,35 @@
-class Dom {}
+class Dom {
+  constructor(selector) {
+    this.$el =
+      typeof selector === 'string'
+        ? document.querySelector(selector)
+        : selector;
+  }
 
-export const $ = () => new Dom();
+  html(html) {
+    if (typeof html === 'string') {
+      this.$el.innerHTML = html;
+      return this;
+    }
+    return this.$el.outerHTML.trim();
+  }
+
+  clear() {
+    this.html('');
+    return this;
+  }
+
+  append(node) {
+    if (node instanceof Dom) node = node.$el;
+    this.$el.append(node);
+    return this;
+  }
+}
+
+export const $ = s => new Dom(s);
 
 $.create = (tagName, className, ...classes) => {
   const el = document.createElement(tagName);
   if (className) el.classList.add(className, ...classes);
-  return el;
+  return $(el);
 };
