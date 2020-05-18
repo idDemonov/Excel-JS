b551edb7d12e8a290d7829c2ed43bc818db7f966
idDemonov <rustamdemonov@gmail.com>
2020-05-18 18:25:25 +0300

    Добавлен обработчик для resize

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -1,10 +1,21 @@
 import { ExcelComponent } from '@core/Excel-component';
 import { createTable } from '@/components/editor/editor.template';
+import { resizeHandler } from '@/components/editor/editor.resize';
 
 export class Editor extends ExcelComponent {
   static classes = ['excel__editor', 'editor'];
 
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown'],
+    });
+  }
+
   toHTML() {
     return createTable(25);
+  }
+
+  onMousedown(event) {
+    if (event.target.dataset.resize) resizeHandler(this.$root, event);
   }
 }
