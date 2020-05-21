655f580206b2b1e914ea4a13cdbca91448ca7970
idDemonov <rustamdemonov@gmail.com>
2020-05-21 04:06:48 +0300

    Функция для создания массива от start до end

--- src/core/utils.js
+++ src/core/utils.js
@@ -1,4 +1,10 @@
-export const capitalizeFirstLetter = string => {
+export const capitalizeFirstLetter = (string) => {
   if (typeof string !== 'string') return '';
   return string && string.replace(string[0], string[0].toUpperCase());
+};
+
+export const range = (start, end) => {
+  if (start > end) [end, start] = [start, end];
+
+  return new Array(end - start + 1).fill('').map((_, ind) => start + ind);
 };
