88a37b0a82eb019d2129663e6b90d431dd65f1bd
idDemonov <rustamdemonov@gmail.com>
2020-05-21 04:03:02 +0300

    Добавлена возможность множ. выбора ячеек

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -2,6 +2,8 @@ import { ExcelComponent } from '@core/Excel-component';
 import { createTable } from '@/components/editor/editor.template';
 import { resizeHandler } from '@/components/editor/editor.resize';
 import { EditorSelection } from '@/components/editor/Editor-selection';
+import { $ } from '@core/dom';
+import { range } from '@core/utils';
 
 export class Editor extends ExcelComponent {
   static classes = ['excel__editor', 'editor'];
@@ -25,6 +27,31 @@ export class Editor extends ExcelComponent {
   }
 
   onMousedown(event) {
-    if (event.target.dataset.resize) resizeHandler(this.$root, event);
+    if (event.target.dataset.resize) {
+      resizeHandler(this.$root, event);
+    } else if (event.target.dataset.id) {
+      const $target = $(event.target);
+      if (event.shiftKey) {
+        const $cells = matrix($target, this.selection.current).map((id) => {
+          return this.$root.find(`[data-id="${id}"]`);
+        });
+        this.selection.selectGroup($cells);
+      } else {
+        this.selection.select($target);
+      }
+    }
   }
 }
+
+const matrix = ($target, $current) => {
+  const target = $target.id(true);
+  const current = $current.id(true);
+
+  const cols = range(current.col, target.col);
+  const rows = range(current.row, target.row);
+
+  return cols.reduce((acc, col) => {
+    rows.forEach((row) => acc.push(`${row}:${col}`));
+    return acc;
+  }, []);
+};
