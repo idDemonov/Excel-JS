53bad75b85a797ab054b48f0c0a5009646febabb
idDemonov <rustamdemonov@gmail.com>
2020-05-21 04:05:49 +0300

    Добавлена возможность множ. выбора ячеек

--- src/components/editor/Editor-selection.js
+++ src/components/editor/Editor-selection.js
@@ -1,12 +1,25 @@
 export class EditorSelection {
   constructor() {
     this.group = [];
+    this.current = null;
   }
   // $el всегда instance класса dom
   select($el) {
+    this.clear();
     $el.addClass('selected');
     this.group.push($el);
+    this.current = $el;
   }
 
-  selectGroup() {}
+  selectGroup($group = []) {
+    this.clear();
+
+    this.group = $group;
+    this.group.forEach(($el) => $el.addClass('selected'));
+  }
+
+  clear() {
+    this.group.forEach(($el) => $el.removeClass('selected'));
+    this.group = [];
+  }
 }
