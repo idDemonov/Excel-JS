8e4c9b8efbf41e215444567df8bf8d5a65b82b35
idDemonov <rustamdemonov@gmail.com>
2020-05-21 22:47:13 +0300

    Вынес обработчики в отдел. файл

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -2,15 +2,16 @@ import { ExcelComponent } from '@core/Excel-component';
 import { createTable } from '@/components/editor/editor.template';
 import { resizeHandler } from '@/components/editor/editor.resize';
 import { EditorSelection } from '@/components/editor/Editor-selection';
-import { $ } from '@core/dom';
-import { range } from '@core/utils';
+import { handlerSelection } from '@/components/editor/editor.selection';
+import { keyboardHandler } from '@/components/editor/editor.keyboard';
 
 export class Editor extends ExcelComponent {
   static classes = ['excel__editor', 'editor'];
 
   constructor($root) {
     super($root, {
-      listeners: ['mousedown'],
+      name: 'Editor',
+      listeners: ['mousedown', 'keydown'],
     });
   }
 
@@ -30,28 +31,11 @@ export class Editor extends ExcelComponent {
     if (event.target.dataset.resize) {
       resizeHandler(this.$root, event);
     } else if (event.target.dataset.id) {
-      const $target = $(event.target);
-      if (event.shiftKey) {
-        const $cells = matrix($target, this.selection.current).map((id) => {
-          return this.$root.find(`[data-id="${id}"]`);
-        });
-        this.selection.selectGroup($cells);
-      } else {
-        this.selection.select($target);
-      }
+      handlerSelection(this.$root, this.selection, event);
     }
   }
+
+  onKeydown(event) {
+    keyboardHandler(event, this.selection, this.$root);
+  }
 }
-
-const matrix = ($target, $current) => {
-  const target = $target.id(true);
-  const current = $current.id(true);
-
-  const cols = range(current.col, target.col);
-  const rows = range(current.row, target.row);
-
-  return cols.reduce((acc, col) => {
-    rows.forEach((row) => acc.push(`${row}:${col}`));
-    return acc;
-  }, []);
-};
