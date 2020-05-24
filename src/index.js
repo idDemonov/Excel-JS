0a4fdd2ebb2c08f5dc29777b77ed2f3534199247
idDemonov <rustamdemonov@gmail.com>
2020-05-24 23:00:09 +0300

    feat(redux): добавил основные методы redux

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import { Toolbar } from '@/components/toolbar/Toolbar';
 import { Titlebar } from '@/components/titlebar/Titlebar';
 import { Formula } from '@/components/formula/Formula';
 import { Editor } from '@/components/editor/Editor';
+import { createStore } from '@/redux/createStore';
+import { rootReducer } from '@/redux/rootReducer';
+
+const store = createStore(rootReducer, {});
 
 const excel = new Excel('#app', {
   components: [Titlebar, Toolbar, Formula, Editor],
