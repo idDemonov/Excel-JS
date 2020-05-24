d9ce8b939b1eea58f204dca5086f47e09f100165
idDemonov <rustamdemonov@gmail.com>
2020-05-25 02:40:16 +0300

    feat(redux): создан initial state

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import { Toolbar } from '@/components/toolbar/Toolbar';
 import { Titlebar } from '@/components/titlebar/Titlebar';
 import { Formula } from '@/components/formula/Formula';
 import { Editor } from '@/components/editor/Editor';
-import { createStore } from '@/redux/createStore';
-import { rootReducer } from '@/redux/rootReducer';
+import { createStore } from '@/redux/create-store';
+import { rootReducer } from '@/redux/root-reducer';
 import { storage } from '@core/utils';
+import { initialState } from '@/redux/initial-state';
 
-const store = createStore(rootReducer, storage('excel-state'));
+const store = createStore(rootReducer, initialState);
 
 store.subscribe((state) => storage('excel-state', state));
 
