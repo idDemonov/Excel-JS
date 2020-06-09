65f5fcce170e375faf83455b397faadc28711ce6
idDemonov <rustamdemonov@gmail.com>
2020-06-09 23:33:07 +0300

    type(Store-sub)

--- src/core/Store-subscriber.ts
+++ src/core/Store-subscriber.ts
@@ -1,17 +1,27 @@
-import { isEqual } from '@core/utils';
+import { isDefined, isEqual, keys } from '@core/utils';
+import { Titlebar } from '@/components/titlebar/Titlebar';
+import { Toolbar } from '@/components/toolbar/Toolbar';
+import { Formula } from '@/components/formula/Formula';
+import { Editor } from '@/components/editor/Editor';
+import { IStoreSubscriber, TState, TStore } from '@/interface';
 
-export class StoreSubscriber {
-  constructor(store) {
+export class StoreSubscriber implements IStoreSubscriber {
+  private prevState: TState;
+  private sub: { unsubscribe: () => void } | null;
+  private store: TStore;
+  constructor(store: TStore) {
     this.store = store;
     this.sub = null;
-    this.prevState = {};
+    this.prevState = {} as TState;
   }
 
-  subscribeComponents(components) {
+  subscribeComponents(
+    components: Array<Titlebar | Toolbar | Formula | Editor>
+  ): void {
     this.prevState = this.store.getState();
 
-    this.sub = this.store.subscribe((state) => {
-      Object.keys(state).forEach((key) => {
+    this.sub = this.store.subscribe((state: TState): void => {
+      keys(state).forEach((key) => {
         if (!isEqual(this.prevState[key], state[key])) {
           components.forEach((component) => {
             if (component.subscribe.includes(key)) {
@@ -25,7 +35,7 @@ export class StoreSubscriber {
     });
   }
 
-  unsubscribeFromStore() {
-    this.sub.unsubscribe();
+  unsubscribeFromStore(): void {
+    if (isDefined(this.sub)) this.sub.unsubscribe();
   }
 }
