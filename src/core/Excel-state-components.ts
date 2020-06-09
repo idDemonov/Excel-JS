aa54b08886c6b19e8f102bc3ea5e0f765d944ae2
idDemonov <rustamdemonov@gmail.com>
2020-06-09 23:34:14 +0300

    type(Excel-state)

--- src/core/Excel-state-components.ts
+++ src/core/Excel-state-components.ts
@@ -1,19 +1,27 @@
 import { ExcelComponent } from '@core/Excel-component';
+import {
+  IDom,
+  IExcelStateComponents,
+  TOptionsElComp,
+  TStylesCell,
+} from '@/interface';
 
-export class ExcelStateComponents extends ExcelComponent {
-  constructor(...args) {
+export class ExcelStateComponents extends ExcelComponent
+  implements IExcelStateComponents {
+  state: TStylesCell | undefined;
+  constructor(...args: [IDom, TOptionsElComp]) {
     super(...args);
   }
 
-  get template() {
+  get template(): string {
     return JSON.stringify(this.state, null, 2);
   }
 
-  initState(initialState = {}) {
+  initState(initialState: TStylesCell): void {
     this.state = { ...initialState };
   }
 
-  setState(newState) {
+  setState(newState: TStylesCell): void {
     this.state = { ...this.state, ...newState };
     this.$root.html(this.template);
   }
