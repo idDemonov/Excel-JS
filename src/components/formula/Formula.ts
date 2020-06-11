2cabd29304d68ff37667e04e99967c9e2587c562
idDemonov <rustamdemonov@gmail.com>
2020-06-11 03:26:18 +0300

    type(Formula)

--- src/components/formula/Formula.ts
+++ src/components/formula/Formula.ts
@@ -1,47 +1,62 @@
 import { ExcelComponent } from '@core/Excel-component';
-import { $ } from '@core/dom';
+import { $ } from '@core/Dom';
+import { IDom, IFormula, TComponentsOptions } from '@/interface';
 
-export class Formula extends ExcelComponent {
-  static classes = ['excel__formula', 'formula'];
+export class Formula extends ExcelComponent implements IFormula {
+  static classes: string[] = ['excel__formula', 'formula'];
+  private $formula: IDom | undefined;
 
-  constructor($root, options) {
+  constructor($root: IDom, options: TComponentsOptions) {
     super($root, {
       name: 'Formula',
-      listeners: ['input', 'keydown'],
       subscribe: ['currentText'],
       ...options,
     });
   }
 
-  toHTML() {
+  toHTML(): string {
     return `
        <div class="formula__label">fx</div>
        <div class="formula__input" contenteditable spellcheck="false"></div>
    `;
   }
 
-  init() {
-    super.init();
+  init(): void {
+    this.$root.on('keydown', this.onKeydown);
+    this.$root.on('input', this.onInput);
 
     this.$formula = this.$root.find('.formula__input');
 
-    this.$$attach('editor:select', ($cell) => {
-      this.$formula.text($cell.dataset.value);
+    this.$$attach<'editor:select'>('editor:select', ($cell) => {
+      if ($cell.dataset.value) this.$formula?.text($cell.dataset.value);
+      else this.$formula?.text($cell.text() as string);
     });
   }
 
-  storeChanged({ currentText }) {
-    this.$formula.text(currentText);
+  storeChanged(changes: Record<string, unknown>): void {
+    this.$formula?.text(changes.currentText as string);
   }
+  // Не InputEvent т.к. это div
+  onInput = ({ target }: Event): void => {
+    if (target instanceof HTMLElement) {
+      const text = $(target).text();
+      if (typeof text === 'string') {
+        this.$$notify<'formula:input'>('formula:input', text);
+      }
+    }
+  };
 
-  onInput({ target }) {
-    this.$$notify('formula:input', $(target).text());
-  }
-
-  onKeydown(event) {
+  onKeydown = (event: KeyboardEvent): void => {
     const keys = ['Enter', 'Tab'];
     if (!keys.includes(event.key)) return;
     event.preventDefault();
     this.$$notify('formula:enter');
+  };
+
+  destroy(): void {
+    super.destroy();
+
+    this.$root.removeEvent('keydown', this.onKeydown);
+    this.$root.removeEvent('input', this.onInput);
   }
 }
